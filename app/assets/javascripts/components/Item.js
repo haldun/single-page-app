51c6cd74a32a75e5f255db7a775e897ccad74230
Haldun Bayhantopcu <hb@haldun.me>
2015-06-04 17:46:02 +0200

    stuff

--- app/assets/javascripts/components/Item.js
+++ app/assets/javascripts/components/Item.js
@@ -1,7 +1,12 @@
 import React from 'react/addons';
+import { Link } from 'react-router';
 
 export default class Item extends React.Component {
   render() {
-    return <li>{this.props.item}</li>;
+    return <li>
+      <Link to="itemDetail" params={{ id: this.props.item.id }}>
+        {this.props.item.text}
+      </Link>
+    </li>;
   }
 }
